6f730506716cb943ca97272799c607646cfac990
Konstantin Bläsi <kblaesi@gmail.com>
2024-09-07 23:09:54 +0200

    .

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="relative flex h-full w-full items-center justify-center">
+	<enhanced:img
+		class="absolute inset-0 z-0 max-h-full min-h-full min-w-full bg-cover bg-center object-cover opacity-80"
+		src="$lib/images/background.jpg"
+		sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+		fetchpriority="high"
+		loading="eager"
+		alt="Background"
+	/>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="relative h-1/2 w-1/2 bg-green-700 bg-opacity-80 p-4 text-white opacity-100">
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
